e24c75eace5e2a8e50d46a3f7bc498f0ea4fadcf
Caitie Phillips <phillips.e.caitlin@gmail.com>
2023-10-10 18:34:19 +1000

    add row and col headers

--- projects/CT_reconstruction/plot_recon.py
+++ projects/CT_reconstruction/plot_recon.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+path = ".\\auto_recon_data\\recons\\"
 
 iterations_lst = [500, 1000, 1500]
 addNoise_lst = [True, False]
@@ -11,8 +12,8 @@ fig_false, ax_false = plt.subplots(nrows=3, ncols=4)
 
 for r, iterations in enumerate(iterations_lst): 
     for c, max_angles in enumerate(angles_lst):
-            file_true = "noise_True_its_{}_angles_{}.npz".format(iterations, max_angles)
-            file_false = "noise_False_its_{}_angles_{}.npz".format(iterations, max_angles)
+            file_true = path + "noise_True_its_{}_angles_{}.npz".format(iterations, max_angles)
+            file_false = path + "noise_False_its_{}_angles_{}.npz".format(iterations, max_angles)
             data_true = np.load(file_true)
             data_false = np.load(file_false)
             recon_true = np.array(data_true["recon"])
@@ -21,8 +22,15 @@ for r, iterations in enumerate(iterations_lst):
             time_false = data_false["time"]
 
             ax_true[r, c].imshow(recon_true)
-            ax_true[r, c].set_title("recon time: {:.5}s".format(time_true))
+            ax_true[r, c].set_xlabel("recon time: {:.5}s".format(time_true))
+            ax_true[r, c].set_ylabel("iterations: {}".format(iterations_lst[r]))
             ax_false[r, c].imshow(recon_false)
-            ax_false[r, c].set_title("recon time: {:.5}s".format(time_false))
+            ax_false[r, c].set_xlabel("recon time: {:.5}s".format(time_false))
+            ax_false[r, c].set_ylabel("iterations: {}".format(iterations_lst[r]))
+
+for col in range(0, 4):
+    ax_true[0, col].set_title("projs: {}".format(angles_lst[col]))
+    ax_false[0, col].set_title("projs: {}".format(angles_lst[col]))
+
 
 plt.show()
